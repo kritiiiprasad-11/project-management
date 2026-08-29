266d1d5ba02b5b70ffd62ee4a6c3e6f0eeee54d6
crackheadd-11 <kittzz2crazy@gmail.com>
2026-08-30 03:28:23 +0530

    Fix Inngest v4 createFunction signature

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,8 +6,7 @@ export const inngest = new Inngest({ id: "project-management" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk/user.created'},
+    {id: 'sync-user-from-clerk', triggers: { event: 'clerk/user.created'} },
     async ({ event })=> {
         const {data} = event
         await prisma.user.create({
@@ -21,8 +20,7 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-from-clerk'},
-    {event: 'clerk/user.deleted'},
+    {id: 'delete-user-from-clerk', triggers: { event: 'clerk/user.deleted'} },
     async ({ event })=> {
         const {data} = event
         await prisma.user.delete({
@@ -35,8 +33,7 @@ const syncUserDeletion = inngest.createFunction(
 
 // Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-    {id: 'update-user-from-clerk'},
-    {event: 'clerk/user.updated'},
+    {id: 'update-user-from-clerk', triggers: {event: 'clerk/user.updated'} },
     async ({event})=> {
         const {data} = event
         await prisma.user.update({
